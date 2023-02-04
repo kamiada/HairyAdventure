a82efeb747db49e6e7e662c301cdc8afb949cb8b
kamiada <kamiada528@gmail.com>
2023-02-04 01:37:32 +0000

    working on set up

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+using Platformer.Mechanics;
+using UnityEngine;
+
+namespace Platformer.Model
+{
+    /// <summary>
+    /// The main model containing needed data to implement a platformer style 
+    /// game. This class should only contain data, and methods that operate 
+    /// on the data. It is initialised with data in the GameController class.
+    /// </summary>
+    [System.Serializable]
+    public class PlatformerModel
+    {
+        /// <summary>
+        /// The virtual camera in the scene.
+        /// </summary>
+        public Cinemachine.CinemachineVirtualCamera virtualCamera;
+
+        /// <summary>
+        /// The main component which controls the player sprite, controlled 
+        /// by the user.
+        /// </summary>
+        public PlayerController player;
+
+        /// <summary>
+        /// The spawn point in the scene.
+        /// </summary>
+        public Transform spawnPoint;
+
+        /// <summary>
+        /// A global jump modifier applied to all initial jump velocities.
+        /// </summary>
+        public float jumpModifier = 1.5f;
+
+        /// <summary>
+        /// A global jump modifier applied to slow down an active jump when 
+        /// the user releases the jump input.
+        /// </summary>
+        public float jumpDeceleration = 0.5f;
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : KinematicObject
 {
+    public float maxSpeed = 7;
+    Vector2 move;
+    SpriteRenderer spriteRenderer;
+    internal Animator animator;
+    public Stamina stamina;
+    public bool controlEnabled = true;
+    public Collider2D collider2d;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +26,16 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        if (controlEnabled)
+        {
+            move.x = Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            move.x = 0;
+        }
+        base.Update();
     }
 }
